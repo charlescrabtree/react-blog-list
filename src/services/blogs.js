fa8140d56f7cebbb29b0ba41a5aeaca2be68ca3a
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-19 13:53:58 -0700

    added blogs.js to services folder

---
+++ src/services/blogs.js
@@ -0,0 +1,8 @@
+import { check } from 'prettier';
+import { client, checkError } from './client';
+
+export async function getBlogs() {
+  const response = await client.from('blogs').select(`*`);
+  
+  return checkError(response);
+}
